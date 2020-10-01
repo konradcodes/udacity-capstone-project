29f5a3569eb263f0fa5e0b33da686bf19768b685
konradcodes <konkordek47@gmail.com>
2020-10-01 20:56:52 +0100

    WHat: adding screenshots folder. Updating readme with references

--- scripts-folder/run_kubernetes.sh
+++ scripts-folder/run_kubernetes.sh
@@ -5,16 +5,13 @@
 # Step 1:
 dockerpath=konradcodes/udacity-dev-ops-capstone-project
 
-# Step 2
 # Run the Docker Hub container with kubernetes
 kubectl run udacity-dev-ops-capstone-project --image=$dockerpath --port=80
 
 
-# Step 3:
 # Listing kubernetes pods
 kubectl get pods
 
 
-# Step 4:
 # Forward the container port to a host
 kubectl expose deployment udacity-dev-ops-capstone-project --type=LoadBalancer --port=80
